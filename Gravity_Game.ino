f9456ec52599371e2e6e88929b703189ef7a8ed5
sirofsky <jacobsurovsky@gmail.com>
2020-09-23 15:51:58 -0700

    2 buckets now works

--- Gravity_Game.ino
+++ Gravity_Game.ino
@@ -8,13 +8,8 @@
 // so the center blink would send angle 0 on its face 0. When a tile recieves a
 // compass angle from a parent, it uses it to compute which one of its own faces
 // is pointing north, and then uses that to send a correct global angle to its children.
-// For example, if a tiel recieves an angle 0 on a face, then it knows the opsite face is
+// For example, if a tile recieves an angle 0 on a face, then it knows the opsite face is
 // pointing north (angle 0).
-
-
-void setup() {
-  // Function intentionally left blank
-}
 
 const byte NO_PARENT_FACE = FACE_COUNT ;   // Signals that we do not currently have a parent
 
@@ -29,13 +24,28 @@ const byte IR_IDLE_VALUE = 7;   // A value we send when we do not know which way
 
 const int LOCKOUT_TIMER_MS = 250;               // This should be long enough for very large loops, but short enough to be unnoticable
 
+//------------------------------------------------------
+
 bool amGod = false;
 byte gravitySignal[6] = {IR_IDLE_VALUE, IR_IDLE_VALUE, IR_IDLE_VALUE, IR_IDLE_VALUE, IR_IDLE_VALUE, IR_IDLE_VALUE};
 byte godFaceOffset = 0;
 
+enum blinkRoles {WALL, BUCKET, SPAWNER};
+byte blinkRole = WALL;
+
+bool bLongPress;
+bool bChangeRole;
+
+
+void setup() {
+  // Function intentionally left blank
+}
+
+
+
 void loop() {
 
-  gravityLoop();
+  setRole();
 
   if (buttonDoubleClicked()) {
     amGod = !amGod;
@@ -52,10 +62,77 @@ void loop() {
   }
 }
 
+void wallLoop() {
+  if (bChangeRole) {
+    blinkRole = BUCKET;
+    bChangeRole = false;
+  }
+
+  setColor(OFF);
+
+  amGod = false;
+
+
+  FOREACH_FACE(f) {
+    //    if (getGravitySignal(getLastValueReceivedOnFace(f)) == 6) {
+    //      setColorOnFace(YELLOW, f);
+    //    }
+
+    if (isBucket(f)) { //do I have a neighbor and are they shouting IM_BUCKET?
+      setColorOnFace(YELLOW, f);
+      byte bucketNeighbor = (f + 2) % 6; //and what about their neighbor?
+      if (isBucket(bucketNeighbor)) {
+        setColor(WHITE);
+        //        bFace = (f + 1) % 6;
+        //        bottomFace = bFace;
+        //        randomWallRole = 10;
+        //        imSwitcher = true;
+        amGod = true; //I get to decide what direction gravity is for the entire game! Yippee!
+      }
+    }
+  }
+
+  gravityLoop();
+
+}
+
+bool isBucket (byte face) {
+  if (!isValueReceivedOnFaceExpired(face)) { //I have a neighbor
+    if (getGravitySignal(getLastValueReceivedOnFace(face)) == 6) { //if the neighbor is a bucket, return true
+      return true;
+    } else if (getGravitySignal(getLastValueReceivedOnFace(face)) != 6) { //if the neighbor isn't a bucket, return false
+      return false;
+    }
+  } else {
+    return false; //or if I don't have a neighbor, that's also a false
+  }
+}
+
+void bucketLoop() {
+  if (bChangeRole) {
+    blinkRole = SPAWNER;
+    bChangeRole = false;
+  }
+
+  setColor(ORANGE);
+
+  FOREACH_FACE(f) {
+    gravitySignal[f] = 6; //6 means bucket, I don't make the rules
+  }
+}
+
+void spawnerLoop() {
+  if (bChangeRole) {
+    blinkRole = WALL;
+    bChangeRole = false;
+  }
+
+  setColor(GREEN);
+}
+
 void gravityLoop() {
-  setColor( OFF );
 
-
+  //  setColor( OFF );
 
   if (amGod) {
 
@@ -67,7 +144,6 @@ void gravityLoop() {
 
       //setValueSentOnFace( f , f );
       gravitySignal[f] = (f + 6 - godFaceOffset) % 6;
-
 
     }
 
@@ -93,30 +169,32 @@ void gravityLoop() {
 
         FOREACH_FACE(f) {
 
-          if (!isValueReceivedOnFaceExpired(f) && (getGravitySignal(getLastValueReceivedOnFace(f)) != IR_IDLE_VALUE) ) {
+          if (!isValueReceivedOnFaceExpired(f)) {
 
-            // Found a parent!
+            if (getGravitySignal(getLastValueReceivedOnFace(f)) < FACE_COUNT) {
 
-            parent_face = f;
+              // Found a parent!
 
-            // Compute the oposite face of the one we are reading from
+              parent_face = f;
 
-            byte our_oposite_face = (f + (FACE_COUNT / 2) ) % FACE_COUNT;
+              // Compute the oposite face of the one we are reading from
 
-            // Grab the compass heading from the parent face we are facing
-            // (this is also compass heading of our oposite face)
+              byte our_oposite_face = (f + (FACE_COUNT / 2) ) % FACE_COUNT;
 
-            byte parent_face_heading = getGravitySignal(getLastValueReceivedOnFace(f));
+              // Grab the compass heading from the parent face we are facing
+              // (this is also compass heading of our oposite face)
 
-            // Ok, so now we know that `our_oposite_face` has a heading of `parent_face_heading`.
+              byte parent_face_heading = getGravitySignal(getLastValueReceivedOnFace(f));
 
-            // Compute which face is our north face from that
+              // Ok, so now we know that `our_oposite_face` has a heading of `parent_face_heading`.
 
-            the_north_face = ( (our_oposite_face + FACE_COUNT) - parent_face_heading) % FACE_COUNT;     // This +FACE_COUNT is to keep it positive
+              // Compute which face is our north face from that
 
-            // I guess we could break here, but breaks are ugly so instead we will keep looking
-            // and use whatever the highest face with a parent that we find.
+              the_north_face = ( (our_oposite_face + FACE_COUNT) - parent_face_heading) % FACE_COUNT;     // This +FACE_COUNT is to keep it positive
 
+              // I guess we could break here, but breaks are ugly so instead we will keep looking
+              // and use whatever the highest face with a parent that we find.
+            }
           }
         }
       }
@@ -173,6 +251,44 @@ void gravityLoop() {
   }
 }
 
-byte getGravitySignal(byte data) {
+
+
+
+void setRole() {
+  if (hasWoken()) {
+    bLongPress = false;
+  }
+
+  if (buttonLongPressed()) {
+    bLongPress = true;
+  }
+
+  if (buttonReleased()) {
+    if (bLongPress) {
+      //now change the role
+      bChangeRole = true;
+      bLongPress = false;
+    }
+  }
+
+  switch (blinkRole) {
+    case WALL:
+      wallLoop();
+      break;
+    case BUCKET:
+      bucketLoop();
+      break;
+    case SPAWNER:
+      spawnerLoop();
+      break;
+  }
+
+  if (bLongPress) {
+    //transition color
+    setColor(WHITE);
+  }
+}
+
+byte getGravitySignal(byte data) { //all the gravity communication happens here
   return (data & 7);
 }
